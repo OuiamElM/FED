9c1a0d794ad0f81570fd5357bf654502e7d6bcb0
OuiamElM <118132232+OuiamElM@users.noreply.github.com>
2022-11-30 23:56:19 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,20 @@
 // JavaScript Document
 console.log("hi");
+
+var menuButton = document.querySelector("header > button");
+var menu = document.querySelector("header nav ul");
+var buttonImage = document.querySelector("header > button > img" );
+
+function menuToggelen(){
+    menu.classList.toggle("menuOpen");
+
+    if(buttonImage.src === "https://www.benjerry.nl/modules/bnj-modules/img/social-icons/menu.svg"){
+        buttonImage.src = "https://assets.codepen.io/274456/ui-icon-cross.svg"
+    } else{
+        buttonImage.src = "https://www.benjerry.nl/modules/bnj-modules/img/social-icons/menu.svg"
+    }
+
+}
+
+menuButton.addEventListener('click', menuToggelen)
+
